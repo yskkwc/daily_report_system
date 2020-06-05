fc4ae4b105ec40bbefdea1d95796f8b3a7ecef58
user <user@usernoMacBook-Air.local>
2020-06-05 21:10:27 +0900

    Report create, Report update

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -60,7 +60,7 @@ public class ReportsUpdateServlet extends HttpServlet {
             r.setReport_date(report_date);
             r.setTitle(request.getParameter("title"));
             r.setContent(request.getParameter("content"));
-            r.setRange(request.getParameter("range"));
+            r.setPublish(request.getParameter("publish"));
 
             r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
